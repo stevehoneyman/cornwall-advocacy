4a56ff87df51f7cc82c55bb046c882077b41aa86
Steve Honeyman <hachimitsu@Steves-MacBook-Pro.local>
2021-01-03 17:47:47 +0000

    basic responsive styles added to header

--- functions.php
+++ functions.php
@@ -11,22 +11,21 @@ function set_theme_scripts() {
 }
 add_action('wp_enqueue_scripts' , 'set_theme_scripts');
 
-// add menus
-// add_theme_support('menus');
+// enable menus in dashboard
+ add_theme_support('menus');
 
-
-function register_theme_menus() {
-	register_nav_menus(
-		array(
-			'primary_nav_pages' => __('Primary Nav Pages'),
-      'primary_nav_cta' => __('Primary Nav CTA'),
-			'primary_nav_sub' => __('Primaty Nav Sub'),
-	    )
-		);
-	}
-
-add_action('init', 'register_theme_menus');
-
+/*
+  function register_theme_menus() {
+  	register_nav_menus(
+  		array(
+  			'primary_nav_pages' => __('Primary Nav Pages'),
+        'primary_nav_cta' => __('Primary Nav CTA'),
+  			'primary_nav_sub' => __('Primaty Nav Sub'),
+  	    )
+  		);
+  	}
+  add_action('init', 'register_theme_menus');
+*/
 
 // enable featured images
 add_theme_support('post-thumbnails');
@@ -34,7 +33,7 @@ add_theme_support('post-thumbnails');
 // enable excerpts
 add_post_type_support( 'page', 'excerpt' );
 
-// show toolbar
+// show toolbar when logged out
 // show_admin_bar( true );
 
 /* TIMBER $context
@@ -44,6 +43,7 @@ function add_to_context( $context ) {
   // So here you are adding data to Timber's context object, i.e...
   // $context['foo'] = 'I am some other typical value set in your functions.php file, unrelated to the menu';
 
+  // set-up menus
   $context['primary_nav_pages']  = new Timber\Menu('Primary');
   $context['primary_nav_cta'] = new Timber\Menu('CTA');
   $context['primary_nav_sub'] = new Timber\Menu('Sub');
