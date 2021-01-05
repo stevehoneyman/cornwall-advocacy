bade7715c33f2018dbce31551310cebd9cd4317e
Steve Honeyman <hachimitsu@Steves-MacBook-Pro.local>
2021-01-05 22:52:30 +0000

    header re-factored, styles tightened up, button added and jQuery handler attached

--- js/scripts.js
+++ js/scripts.js
@@ -2,75 +2,12 @@
 var dateTime = new Date();
 console.log('loading from scripts.js from Cornwall Advocacy on: ' + dateTime);
 
-jQuery(document).ready(function($){ // start doc ready
+jQuery(document).ready(function($){
 
-  /* start scripts
-  ------------------------------------------------------------------------- */
-
-	/* navigation
+  /* navigation
 	------------------------------------------------------------------------- */
-	// controls for main nav
-	// toggle visibility
-	$('.navicon').on('click', function() {
-		// console.log('click fired...')
-		$(this).next().toggleClass('show');
+  $('.menu').on('click', function() {
+		console.log('click fired...')
 	});
-
-	/* sliders
-	------------------------------------------------------------------------- */
-	$('.basic-slider').slick({
-		//  setting-name: setting-value
- 	}); // end basic
-
-	$('.fade-slider').slick({
-     	// dots: true,
-     	// infinite: true,
-     	speed: 500,
-     	fade: true,
-     	cssEase: 'linear'
-    }); // end fade
-
-	$('.multiple-slider').slick({
-    	//infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1
-    }); // end multiple
-
-	/* accordians - faqs
-	------------------------------------------------------------------------- */
-	$('.faq-question').click(function() {
-		$(this).next().toggleClass('show');
-	});
-
-	/* scroll in
-	------------------------------------------------------------------------- */
-  (function($) {
-			$.fn.visible = function(partial) {
-
-			var $t            = $(this),
-			  $w            = $(window),
-			  viewTop       = $w.scrollTop(),
-			  viewBottom    = viewTop + $w.height(),
-			  _top          = $t.offset().top,
-			  _bottom       = _top + $t.height(),
-			  compareTop    = partial === true ? _bottom : _top,
-			  compareBottom = partial === true ? _top : _bottom;
-
-			return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-
- 		};
-	})(jQuery);
-
-	$(window).scroll(function(event) {
-		$(".module").each(function(i, el) {
-			var el = $(el);
-			if (el.visible(true)) {
-			el.addClass("come-in");
-			}
-		});
-	});
-
-	/* end scripts
-	------------------------------------------------------------------------- */
 
 }); // close doc ready
