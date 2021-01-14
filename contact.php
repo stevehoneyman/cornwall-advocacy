c5fe8d767f854ca98ddac1c28b0f532b7e39d526
Steve Honeyman <hachimitsu@Steves-MacBook-Pro.local>
2021-01-14 10:31:28 +0000

    contact form context, php and twig files added

--- contact.php
+++ contact.php
@@ -2,5 +2,14 @@
   /* Template Name: Contact */
 	$context = Timber::get_context();
 	$context['post'] = new Timberpost();
+  $context['forms'] = [
+    'contact' => new \Timber\FunctionWrapper('gravity_form', [1, false] ),
+  ];
   Timber::render('contact.twig', $context);
+
+  /*
+  $context['forms'] = [
+  			'contact' => new \Timber\FunctionWrapper('gravity_form', [1, false, false, false, null, true, 100] ),
+  ];
+  */
 ?>
