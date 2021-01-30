fb1b09532c44d049d4330066043310873e76f645
Steve Honeyman <hachimitsu@Steves-MacBook-Pro.local>
2021-01-30 15:04:01 +0000

    add slick and set-up testimonials slider

--- functions.php
+++ functions.php
@@ -2,12 +2,15 @@
 // add stylesheet(s)
 function set_theme_styles() {
 	wp_enqueue_style( 'main_stylesheet' , get_template_directory_uri() . '/css/app.css?v=' .time() );
+  wp_enqueue_style( 'slick_theme' , get_template_directory_uri() . '/css/slick/slick-theme.css' );
+	wp_enqueue_style( 'slick' , get_template_directory_uri() . '/css/slick/slick.css' );
 }
 add_action('wp_enqueue_scripts', 'set_theme_styles');
 
 // add scripts
 function set_theme_scripts() {
 	wp_enqueue_script( 'main_script', get_template_directory_uri() . '/js/scripts.js', array('jquery') , '' , true);
+  wp_enqueue_script( 'slick_js', get_template_directory_uri() . '/js/slick.min.js', '' , '' , true);  
 }
 add_action('wp_enqueue_scripts' , 'set_theme_scripts');
 
--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,19 @@ jQuery(document).ready(function($){
 
   /* navigation
 	------------------------------------------------------------------------- */
+  // menu toggle
   $('.menu').on('click', function() {
 		$('.primary-nav').toggleClass('nav-open');
 	});
 
+  // accordian
   $('.item-title-holder').on('click', function() {
     $(this).toggleClass('inherit-animation');
     $(this).next('.item-content').toggleClass('accordian-open');
-
   });
+
+  // testimonial slider
+  $('.testimonial-slider').slick({
+		//  setting-name: setting-value
+ 	}); // end basic
 }); // close doc ready
