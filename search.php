584fc2aa48c33747deda3f9113944dc54c5ceeca
Steve Honeyman <hachimitsu@Steves-MacBook-Pro-2.local>
2021-02-20 16:33:55 +0000

    search.php added

---
+++ search.php
@@ -0,0 +1,6 @@
+<?php
+  $context = Timber::context();
+  $context['title'] = 'Search results for ' . get_search_query();
+  $context['posts'] = new Timber\PostQuery();
+  Timber::render( 'search.twig', $context );
+?>
